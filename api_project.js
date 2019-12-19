61cb12c00163677352c955bc8e4130ed4a93588d
n0vad3v <n0vad3v@riseup.net>
2019-12-19 23:15:49 +0800

    Init

---
+++ api_project.js
@@ -0,0 +1,24 @@
+define({
+  "name": "SMMS API",
+  "version": "2.0.0",
+  "description": "API v2 Documentation",
+  "title": "SMMS v2 API Docs",
+  "order": [
+    "Auth",
+    "User",
+    "Image",
+    "Album"
+  ],
+  "footer": {
+    "content": "<p>请勿上传违反中国大陆和香港法律的图片，违者后果自负。Copyright Ⓒ 2019</p>\n<p>xTom GmbH. All rights reserved.</p>\n"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-12-16T08:24:46.783Z",
+    "url": "http://apidocjs.com",
+    "version": "0.19.0"
+  }
+});
