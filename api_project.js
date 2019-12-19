6f09f1e255098821e9bd0cf8e9d5dbcfa46fdcef
n0vad3v <n0vad3v@riseup.net>
2019-12-20 00:22:58 +0800

    Update

--- api_project.js
+++ api_project.js
@@ -9,15 +9,12 @@ define({
     "Image",
     "Album"
   ],
-  "footer": {
-    "content": "<p>请勿上传违反中国大陆和香港法律的图片，违者后果自负。Copyright Ⓒ 2019</p>\n<p>xTom GmbH. All rights reserved.</p>\n"
-  },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-12-16T08:24:46.783Z",
+    "time": "2019-12-19T16:22:44.684Z",
     "url": "http://apidocjs.com",
     "version": "0.19.0"
   }
